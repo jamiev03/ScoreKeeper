ff2acaa3591db78453feccd5cb14450c6ee0951a
Jamie <jamie03.v@gmail.com>
2021-03-12 21:28:52 -0500

    reset button click event and function added, resets scores and displays, set isGameOver to back to false

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
+const reset = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
@@ -19,13 +20,19 @@ p1Button.addEventListener("click", () => {
 });
 
 p2Button.addEventListener("click", () => {
-    if (!isGameOver) {
-      p2Score += 1;
-      if (p2Score === winningScore) {
-        isGameOver = true;
-      }
-      p2Display.textContent = p2Score;
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
     }
-  });
+    p2Display.textContent = p2Score;
+  }
+});
 
-
+reset.addEventListener("click", () => {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+});
