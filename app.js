875a96b2a9a42211047136da4ea9b020da69eb68
Jamie <jamie03.v@gmail.com>
2021-03-14 12:50:32 -0400

    add selector for setting score, create winningScore variable

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector("#p2Button");
 const reset = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector('#playTo')
 
 let p1Score = 0;
 let p2Score = 0;
